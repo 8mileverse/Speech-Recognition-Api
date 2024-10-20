4f2cd4d20c0485e4f4525024866cb828bc1dcd0f
8mileverse <Ayodelu4@gmail.com>
2024-10-20 20:01:25 +0100

    rockect ship

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
-const SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
-
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const rec = new SpeechRecognition();
 rec.lang = "en-US";
-rec.continuous = true;
+rec.continuous = true; // Set to true for continuous listening
+
+const acceptedColors = [
+  "red", "green", "blue", "yellow", "orange", "purple", 
+  "pink", "brown", "black", "white", "grey", "cyan", 
+  "magenta", "navy", "teal", "lime", "violet", "indigo",
+  "gold", "silver", "coral", "turquoise", "beige", "lavender", 
+  "maroon", "salmon", "plum", "orchid", "tan", "olive", 
+  "peach", "chocolate", "ivory", "khaki", "azure", "fuchsia",
+  "mint", "sienna", "apricot", "emerald", "scarlet", "amber",
+  "burgundy", "crimson", "chartreuse", "denim", "wheat", "periwinkle",
+  "amethyst", "aqua", "bronze", "ruby", "sapphire", "limegreen",
+  "slateblue", "forestgreen", "skyblue", "springgreen", "midnightblue",
+  "lightcoral", "lightgoldenrodyellow", "mediumslateblue", "firebrick", 
+  "tomato", "dodgerblue"
+];
 
 rec.onresult = function (e) {
-  // console.log(e.results[0][0].transcript);
-  const acceptedColors = [
-    "red", "green", "blue", "yellow", "orange", "purple", 
-    "pink", "brown", "black", "white", "grey", "cyan", 
-    "magenta", "navy", "teal", "lime", "violet", "indigo",
-    "gold", "silver", "coral", "turquoise", "beige", "lavender", 
-    "maroon", "salmon", "plum", "orchid", "tan", "olive", 
-    "peach", "chocolate", "ivory", "khaki", "azure", "fuchsia",
-    "mint", "sienna", "apricot", "emerald", "scarlet", "amber",
-    "burgundy", "crimson", "chartreuse", "denim", "wheat", "periwinkle",
-    "amethyst", "aqua", "bronze", "ruby", "sapphire", "limegreen",
-    "slateblue", "forestgreen", "skyblue", "springgreen", "midnightblue",
-    "lightcoral", "lightgoldenrodyellow", "mediumslateblue", "firebrick", 
-    "tomato", "dodgerblue"
-  ];
-
-  for (i = e.resultIndex; i < e.results.length; i++) {
+  for (let i = e.resultIndex; i < e.results.length; i++) {
     const script = e.results[i][0].transcript.toLowerCase().trim();
     if (acceptedColors.includes(script)) {
       document.body.style.backgroundColor = script; // Change background color
-    }else{
-        alert("Sorry, I couldn't recognize that color. Try Again"); // Alert if color not recognized
+    } else {
+      alert("Sorry, I couldn't recognize that color. Try Again"); // Alert if color not recognized
     }
   }
 };
-rec.start();
+
+// Restart recognition when it ends
+rec.onend = function () {
+  setTimeout(() => {
+    rec.start();
+  }, 500); // Optional delay to avoid rapid restarts
+};
+
+// Add button interaction for mobile devices
+document.getElementById('startRecognition').addEventListener('click', function() {
+  rec.start(); // Start speech recognition after user interaction
+});
